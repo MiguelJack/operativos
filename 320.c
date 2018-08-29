d735d15d9d405dde50307758b53ffe0daa9cf4a2
MiguelJack <miguelfenix16@gmail.com>
2018-08-28 19:44:52 -0600

    Add files via upload

---
+++ 320.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#define MIN_PID 300
+#define MAX_PID 5000
+
+int pidsLength = 4700;
+int pids[4700];
+
+int main(int argc, char const *argv[])
+{
+	if (allocate_map() == 1){
+		for (int i = 0; i < 20; ++i)
+		{
+			newProcess();
+		}
+		release_pid(302);
+		newProcess();
+	}
+	
+	
+	return 0;
+}
+
+int newProcess(){
+	int pid = allocate_pid();
+	if (pid != -1){
+		printf("%d\n", pid);
+	}else{
+		printf("ningun pid libre\n");
+	}
+}
+
+int allocate_map(void){
+	for (int i = 1; i <= pidsLength; ++i)
+	{
+		pids[i] = 0;
+	}
+	return 1;
+}
+
+int allocate_pid(void){
+	int pid = -1;
+	for (int i = 1; i <= pidsLength; ++i)
+	{
+		if (pids[i] == 0)
+		{
+			pid = i+MIN_PID;
+			pids[i] = 1;
+			break;
+		}
+	}
+	return pid;
+}
+
+void release_pid(int pid){
+	pids[pid - MIN_PID] = 0;
+}
+
